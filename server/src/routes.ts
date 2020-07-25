1592d436f93b7d9d9d9118920f5854f5b68cd353
Gustavo Sales <manoel0gustavo@gmail.com>
2020-07-24 22:29:19 -0300

    feat: SERVER - create list items route

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,10 +1,20 @@
 import express from 'express';
+import knex from './database/connection';
 
 const router = express.Router();
 
 
-router.get('/', (req, res) => {
-    return res.json({ message: 'OK' });
+router.get('/items', async (req, res) => {
+    const items = await knex('items').select('*');
+
+    const serializedItems = items.map((item) => {
+        return {
+            title: item.title,
+            image_url: `http:localhost:3333/uploads/${item.image}`,
+        }
+    })
+
+    return res.json(serializedItems);
 });
 
 

4abea8584c04ad41556f339f1006ab5a37b6290c
Gustavo Sales <manoel0gustavo@gmail.com>
2020-08-09 11:52:44 -0300

    feat: WEB - create CreatePoint page form

--- web/src/pages/CreatePoint/index.tsx
+++ web/src/pages/CreatePoint/index.tsx
@@ -1,9 +1,118 @@
 import React from 'react';
-
+import logo from '../../assets/logo.svg';
+import './styles.css';
+import { Link } from 'react-router-dom';
+import { FiArrowLeft } from 'react-icons/fi';
 
 const CreatePoint = () => {
     return(
-        <h1>Hello Create POint</h1>
+        <div id="page-create-point">
+            <header>
+                <img src={logo} alt="Ecoleta"/>
+
+                <Link to="/">
+                    <FiArrowLeft />
+                    Voltar para home
+                </Link>
+            </header>
+
+            <form>
+                <h1>Cadastro do ponto <br/> de coleta </h1>
+
+                <fieldset>
+                    <legend>
+                        <h2>Dados</h2>
+                    </legend>
+
+                    <div className="field">
+                        <label htmlFor="name">Nome da entidade</label>
+                        <input 
+                            type="text"
+                            name="name"
+                            id="name"    
+                        />
+                    </div>
+
+                    <div className="field-group">
+                        <div className="field">
+                            <label htmlFor="email">Email</label>
+                            <input 
+                                type="text"
+                                name="email"
+                                id="email"    
+                            />
+                        </div>
+                        <div className="field">
+                            <label htmlFor="whatsapp">Whatsapp</label>
+                            <input 
+                                type="text"
+                                name="whatsapp"
+                                id="whatsapp"    
+                            />
+                        </div>
+                    </div>
+                </fieldset>
+
+                <fieldset>
+                    <legend>
+                        <h2>Endereço</h2>
+                        <span>Selecione o endereço no mapa</span>
+                    </legend>
+                </fieldset>
+
+                <div className="field-group">
+                    <div className="field">
+                        <label htmlFor="uf">Estado (UF)</label>
+                        <select name="uf" id="uf">
+                            <option value="0">Selecione uma UF</option>
+                        </select>
+                    </div>
+                    <div className="field">
+                        <label htmlFor="city">Cidade</label>
+                        <select name="city" id="city">
+                            <option value="0">Selecione uma cidade</option>
+                        </select>
+                    </div>
+                </div>
+
+                <fieldset>
+                    <legend>
+                        <h2>Ítens de coleta</h2>
+                    </legend>
+
+                    <ul className="items-grid">
+                        <li>
+                            <img src="http://localhost:3333/uploads/oleo.svg" alt="Teste"/>
+                            <span>Oléo de Cozinha</span>
+                        </li>
+                        <li className="selected">
+                            <img src="http://localhost:3333/uploads/oleo.svg" alt="Teste"/>
+                            <span>Oléo de Cozinha</span>
+                        </li>
+                        <li>
+                            <img src="http://localhost:3333/uploads/oleo.svg" alt="Teste"/>
+                            <span>Oléo de Cozinha</span>
+                        </li>
+                        <li>
+                            <img src="http://localhost:3333/uploads/oleo.svg" alt="Teste"/>
+                            <span>Oléo de Cozinha</span>
+                        </li>
+                        <li>
+                            <img src="http://localhost:3333/uploads/oleo.svg" alt="Teste"/>
+                            <span>Oléo de Cozinha</span>
+                        </li>
+                        <li>
+                            <img src="http://localhost:3333/uploads/oleo.svg" alt="Teste"/>
+                            <span>Oléo de Cozinha</span>
+                        </li>
+                    </ul>
+                </fieldset>
+
+                <button type="submit">
+                    Cadastrar ponto de coleta
+                </button>
+            </form>
+        </div>
     );
 }
 

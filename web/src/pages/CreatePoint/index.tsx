e7f8b43dabfe6a4b42aad322ae8bf96246d6341e
Gustavo Sales <manoel0gustavo@gmail.com>
2020-08-11 11:45:55 -0300

    feat: WEB - connect CreatePoint form submition to the api to create point

--- web/src/pages/CreatePoint/index.tsx
+++ web/src/pages/CreatePoint/index.tsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState, ChangeEvent, FormEvent } from 'react';
 import logo from '../../assets/logo.svg';
 import './styles.css';
-import { Link } from 'react-router-dom';
+import { Link, useHistory } from 'react-router-dom';
 import { FiArrowLeft } from 'react-icons/fi';
 import { Map, TileLayer, Marker } from 'react-leaflet';
 import axios from 'axios';
@@ -24,6 +24,8 @@ interface IBGECityResponse {
 }
 
 const CreatePoint = () => {
+    const history = useHistory();
+
     const [items, setItems] = useState<Item[]>([]);
     const [ufs, setUfs] = useState<string[]>([]);
     const [cities, setCities] = useState<string[]>([]);
@@ -108,7 +110,7 @@ const CreatePoint = () => {
         }
     }
 
-    function handleSubmit(event: FormEvent) {
+    async function handleSubmit(event: FormEvent) {
         event.preventDefault();
 
         const { name, email, whatsapp } = formData;
@@ -128,7 +130,10 @@ const CreatePoint = () => {
             items,
         }
 
-        console.log(data);
+        await api.post('points', data);
+
+        alert('Ponto de coleta criado');
+        history.push('/');
     }
 
     return(

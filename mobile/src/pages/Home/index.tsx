4c4c73da433631c4625fb86fb4e759315296e977
Gustavo Sales <manoel0gustavo@gmail.com>
2020-08-14 12:57:00 -0300

    feat: MOBILE - add inputs of city and UF in Home page

--- mobile/src/pages/Home/index.tsx
+++ mobile/src/pages/Home/index.tsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import { View, Text, Image, ImageBackground } from 'react-native';
+import React, { useState } from 'react';
+import { View, Text, Image, ImageBackground, TextInput, KeyboardAvoidingView, Platform } from 'react-native';
 import { RectButton } from 'react-native-gesture-handler';
 import { Feather } from '@expo/vector-icons';
 import styles from './styles';
@@ -7,12 +7,15 @@ import { useNavigation } from '@react-navigation/native';
 
 const Home = () => {
     const { navigate } = useNavigation();
+    const [city, setCity] = useState('');
+    const [uf, setUf] = useState('');
 
     function handleNavigateToPoints() {
-        navigate("Points");
+        navigate("Points", { uf, city });
     }
 
     return(
+        <KeyboardAvoidingView style={{flex: 1}} behavior={Platform.OS==='ios' ? 'padding' : undefined}>
         <ImageBackground 
             source={require('../../assets/home-background.png')} 
             style={styles.container}
@@ -27,6 +30,23 @@ const Home = () => {
             </View>
 
             <View style={styles.footer}>
+                <TextInput 
+                    style={styles.input}
+                    placeholder="Digite a UF"
+                    value={uf}
+                    maxLength={2}
+                    autoCapitalize="characters"
+                    autoCorrect={false}
+                    onChangeText={text => setUf(text)}
+                />
+                <TextInput 
+                    style={styles.input}
+                    placeholder="Digite a cidade"
+                    value={city}
+                    autoCorrect={false}
+                    onChangeText={text => setCity(text)}
+                />
+
                 <RectButton style={styles.button} onPress={handleNavigateToPoints}>
                     <View style={styles.buttonIcon}>
                         <Feather name="arrow-right" color="#fff" size={24}/>
@@ -38,6 +58,7 @@ const Home = () => {
             </View>
 
         </ImageBackground>
+        </KeyboardAvoidingView>
     );
 }
 
